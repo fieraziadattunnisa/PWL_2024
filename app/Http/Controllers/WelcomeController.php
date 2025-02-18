9020328eb6991f915c4046d52562cb478bfb6646
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-02-18 13:37:21 +0700

    J2_Praktikum 3_Langkah 12&13

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
     }
 
     public function greeting(){ 
-    return view('blog.hello', ['name' => 'Fiera']); 
+        return view('blog.hello') 
+            ->with('name','Fiera') 
+            ->with('occupation','Astronaut'); 
     }
 }
