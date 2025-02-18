7ad1ca0969681847d566f024a221aa2f154decc0
Fiera Ziadattun Nisa <fieraziadattunnisa@gmail.com>
2025-02-18 12:54:23 +0700

    J2_Praktikum 3_ Langkah 1&2

--- routes/web.php
+++ routes/web.php
@@ -84,6 +84,10 @@ Route::resource('photos', PhotoController::class);
 Route::resource('photos', PhotoController::class)->only([ 
     'index', 'show' 
     ]); 
-    Route::resource('photos', PhotoController::class)->except([ 
+Route::resource('photos', PhotoController::class)->except([ 
     'create', 'store', 'update', 'destroy' 
     ]);
+
+Route::get('/greeting', function () { 
+    return view('hello', ['name' => 'Fiera']); 
+});
